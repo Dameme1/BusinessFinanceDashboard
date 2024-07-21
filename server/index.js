f481a2ca457188d8d23fdf8eeb59749437d01776
Dameme1 <tameemisone786@gmail.com>
2024-07-21 18:07:18 -0400

    adding in backend functionality for products, adding data into mongodb database, and adding new API call into front-end api.ts file

--- server/index.js
+++ server/index.js
@@ -6,8 +6,10 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js"
-import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js";
+import productRoutes from "./routes/product.js"
+import Product from "./models/Product.js"
+import KPI from "./models/KPI.js"; 
+import { kpis, products } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -22,6 +24,7 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
 
 console.log("hello")
 
@@ -39,5 +42,6 @@ mongoose
         /* ADD DATA ONE TIME ONLY OR AS NEEDED */
         // await mongoose.connection.db.dropDatabase(); 
         // KPI.insertMany(kpis);
+        // Product.insertMany(products);
     })
     .catch((error) => console.log(`${error} did not connect`))
